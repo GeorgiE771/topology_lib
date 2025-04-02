7b35b78c715a52c80e880b9bfcb7b594f7ecac0b
GeorgiE771 <114616655+GeorgiE771@users.noreply.github.com>
2025-04-02 14:19:11 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,11 @@ sys.path.insert(0, os.path.abspath('..'))
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
+
+html_theme = 'sphinx_rtd_theme'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-html_theme = 'sphinx_rtd_theme'
